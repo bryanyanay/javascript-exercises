c6d53eb42d96c891602f54cf7067c597add19c8d
Bryan Yan <tyrone.yan@hotmail.com>
2022-04-15 16:25:55 -0400

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start !== "number" || typeof end !== "number") return "ERROR";
+    if (start > end) [start, end] = [end, start]; // swap them so that start <= end
+    if (start < 0) return "ERROR"; // we only need to check start, since if start >= 0 then end >= 0
 
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) sum += i;
+    return sum;
 };
 
 // Do not edit below this line
