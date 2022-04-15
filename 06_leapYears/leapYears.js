ee7bd166cd66ed7afd9f41e41bcde16653c8322c
Bryan Yan <tyrone.yan@hotmail.com>
2022-04-15 16:36:52 -0400

    Complete leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // the Boolean() constructor is to convert any number to bool
+    return Boolean(!(year%4) && ((year%100) || !(year%400))); 
 };
 
 // Do not edit below this line
