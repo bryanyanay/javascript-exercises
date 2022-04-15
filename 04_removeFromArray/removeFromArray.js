1dd11a4d16bcf0bf5da35daf74a8908def0291b2
Bryan Yan <tyrone.yan@hotmail.com>
2022-04-15 16:14:21 -0400

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+    let newArray = [...array]; // spread syntax
+    for (const item of toRemove) 
+        if (newArray.indexOf(item) !== -1) newArray.splice(newArray.indexOf(item), 1); 
+    return newArray;
 };
 
 // Do not edit below this line
