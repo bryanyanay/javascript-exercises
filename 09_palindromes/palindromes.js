bb4730292ccd64ac71831d5046de7d419a0a13ba
Bryan Yan <tyrone.yan@hotmail.com>
2022-04-18 16:05:57 -0400

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    phrase = phrase.replaceAll(" ", "");
+    phrase = phrase.replaceAll("!", "");
+    phrase = phrase.replaceAll(",", "");
+    phrase = phrase.replaceAll(".", "");
+    // this is a bad solution for stripping spaces and punctuation, probably use regex??
 
+    phrase = phrase.toLowerCase();
+
+    console.log(phrase);
+
+    return (phrase.slice(0, Math.floor(phrase.length/2)) 
+            === phrase.slice(Math.ceil(phrase.length/2))
+                    .split("").reverse().join(""));
 };
 
 // Do not edit below this line
