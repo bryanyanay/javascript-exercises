b1b3acbebbed4848594b95b4b0cf4d3810665665
Bryan Yan <tyrone.yan@hotmail.com>
2022-04-18 16:27:53 -0400

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+function getAge(person) {
+    if ("yearOfDeath" in person)
+        return person.yearOfDeath - person.yearOfBirth;
+    return (new Date()).getFullYear() - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((currOldest, newPerson) => 
+            (getAge(currOldest) < getAge(newPerson) ? newPerson : currOldest));
 };
 
 // Do not edit below this line
