c7ebd71c57ec55b2bb3f03f3c1bdeab8c1548d10
Bryan Yan <tyrone.yan@hotmail.com>
2022-04-18 16:13:43 -0400

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n;
+    if (n < 0) return "OOPS";
 
+    // this sol has very poor readability lmao
+    if (n===1) return 1;
+    let [a, b] = [1, 1];
+    --n;
+    while (--n) [a, b] = [b, a+b];
+    return b;
 };
 
 // Do not edit below this line
